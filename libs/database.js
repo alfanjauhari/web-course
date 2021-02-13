4de41e992c940f7444a631a1ac5870203dafe453
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2021-02-13 21:05:44 +0700

    connect to mongodb cloud

--- libs/database.js
+++ libs/database.js
@@ -1,12 +1,20 @@
 const { connect } = require('mongoose');
 
+const dbURI = "mongodb://yosinau_admin:yosinauapp@cluster0-shard-00-00.eopfp.mongodb.net:27017,cluster0-shard-00-01.eopfp.mongodb.net:27017,cluster0-shard-00-02.eopfp.mongodb.net:27017/yosinau?ssl=true&replicaSet=atlas-zydaqp-shard-0&authSource=admin&retryWrites=true&w=majority";
+
 async function connectDb() {
-  await connect(process.env.DB_HOST, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  });
+  try{
+    await connect(dbURI,
+      {useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+    console.log("MongoDB Connected!")
+  }catch(err){
+    console.error(err.message);
+    process.exit(1);
+  }
 }
 
 module.exports = {
